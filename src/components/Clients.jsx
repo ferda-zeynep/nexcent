f7a61c7bb907a51d00d819f385218a5ff6320593
ferda-zeynep <ferdazeynepcapa@gmail.com>
2024-09-06 18:38:32 -1200

    fix: normal path

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,11 +1,3 @@
-import logo1 from "../../public/assets/logo1.svg";
-import logo2 from "../../public/assets/logo2.svg";
-import logo3 from "../../public/assets/logo3.svg";
-import logo4 from "../../public/assets/logo4.svg";
-import logo5 from "../../public/assets/logo5.svg";
-import logo6 from "../../public/assets/logo6.svg";
-import logo7 from "../../public/assets/logo7.svg";
-
 const Clients = () => {
   return (
     <div className="border-neutral-800 pb-24">
@@ -15,13 +7,13 @@ const Clients = () => {
       </p>
 
       <div className="flex justify-between md:w-[85%] my-9 mx-14">
-        <img src={logo1} alt="logo1" />
-        <img src={logo2} alt="logo2" />
-        <img src={logo3} alt="logo3" />
-        <img src={logo4} alt="logo4" />
-        <img src={logo5} alt="logo5" />
-        <img src={logo6} alt="logo6" />
-        <img src={logo7} alt="logo7" />
+        <img src="public/assets/logo1.svg" alt="logo1" />
+        <img src="public/assets/logo2.svg" alt="logo2" />
+        <img src="public/assets/logo3.svg" alt="logo3" />
+        <img src="public/assets/logo4.svg" alt="logo4" />
+        <img src="public/assets/logo5.svg" alt="logo5" />
+        <img src="public/assets/logo6.svg" alt="logo6" />
+        <img src="public/assets/logo7.svg" alt="logo7" />
       </div>
     </div>
   );
